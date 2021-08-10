cef6aa3d658dc537959775e80a49f721d971e7c9
Aleksandr <gvardianez@yandex.ru>
2021-08-10 19:30:54 +0300

    Удаление ДЗ 3

--- core/src/com/mygdx/game/screen/MenuScreen.java
+++ core/src/com/mygdx/game/screen/MenuScreen.java
@@ -5,30 +5,22 @@ import com.badlogic.gdx.math.Vector2;
 import com.mygdx.game.base.BaseScreen;
 import com.mygdx.game.math.Rect;
 import com.mygdx.game.sprite.Background;
-import com.mygdx.game.sprite.Logo;
 
 public class MenuScreen extends BaseScreen {
 
     private Texture bg;
     private Background background;
-    private Texture lg;
-    private Logo logo;
-    private Vector2 pos;
 
     @Override
     public void show() {
         super.show();
         bg = new Texture("textures/bg.png");
         background = new Background(bg);
-        lg = new Texture("badlogic.jpg");
-        logo = new Logo(lg);
-        pos = new Vector2();
     }
 
     @Override
     public void resize(Rect worldBounds) {
         background.resize(worldBounds);
-        logo.resize(worldBounds);
     }
 
     @Override
@@ -42,23 +34,19 @@ public class MenuScreen extends BaseScreen {
     public void dispose() {
         super.dispose();
         bg.dispose();
-        lg.dispose();
     }
 
     private void update(float delta) {
-        logo.update(delta);
     }
 
     private void draw() {
         batch.begin();
         background.draw(batch);
-        logo.draw(batch);
         batch.end();
     }
 
     @Override
     public boolean touchDown(Vector2 touch, int pointer, int button) {
-        logo.touchDown(touch, pointer, button);
         return false;
     }
 }
